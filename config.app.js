c4ed0a8bb11ce92ec7adabd7f440c5be42d0df5b
Thomas Thelen <tommythelen@gmail.com>
2022-02-24 18:26:17 -0800

    Replace 'cegis' ontology usage with 'usgs'. Fix a typo in the environmental variable for the URI path and move the decleration of http/https to the deployment file

--- config.app.js
+++ config.app.js
@@ -1,8 +1,7 @@
 
-const S_DATA_HOST = 'stage.gnis-ld.org';
-// const S_DATA_PATH = process.env.USGS_DATA_PATH || 'stage.gnis-ld.org';
+const S_DATA_HOST = process.env.DEPLOYMENT_HOST || 'http://gnis-ld.org';
 
-const P_DATA_URI = `https://${S_DATA_HOST}`;
+const P_DATA_URI = `${S_DATA_HOST}`;
 const P_RDF_URI = `${P_DATA_URI}/lod`;
 const P_GEOM_URI = `${P_DATA_URI}/geometry`;
 
@@ -20,15 +19,12 @@ module.exports = {
 		geosparql: 'http://www.opengis.net/ont/geosparql#',
 		ago: 'http://awesemantic-geo.link/ontology/',
 		geonames: 'http://sws.geonames.org/',
-
 		usgs: `${P_RDF_URI}/usgs/ontology/`,
 		gnis: `${P_RDF_URI}/gnis/ontology/`,
 		gnisf: `${P_RDF_URI}/gnis/feature/`,
 		'gnisf-alias': `${P_RDF_URI}/gnis/feature-alias/`,
 		nhd: `${P_RDF_URI}/nhd/ontology/`,
 		nhdf: `${P_RDF_URI}/nhd/feature/`,
-		cegis: `${P_RDF_URI}/cegis/ontology/`,
-		cegisf: `${P_RDF_URI}/cegis/feature/`,
 		'usgeo-point': `${P_GEOM_URI}/point/`,
 		'usgeo-multipoint': `${P_GEOM_URI}/multipoint/`,
 		'usgeo-linestring': `${P_GEOM_URI}/linestring/`,
